744b9c4046813232c6d3d3f87d3ca84bf62d7ff3
ricky <rickychan0528@outlook.com>
2023-06-07 23:19:38 +0800

    将cr打包成服务以供使用

--- compile_server/compile_server.cc
+++ compile_server/compile_server.cc
@@ -1,32 +1,35 @@
 #include "compile_run.hpp"
+#include "../comm/httplib.h"
 
 using namespace ns_compile_and_run;
+using namespace httplib;
 
-int main()
+void Usage(std::string proc)
 {
-    std::string in_json;
+    std::cerr << "Usage: " << "\n\t" << proc << " port" << std::endl;
+}
 
-    Json::Value in_value;
-    in_value["code"] = R"(#include <iostream>
-int main()
+int main(int argc, char *argv[])
 {
-    std::cout << "test" << std::endl;
-    aaaaaa
-    return 0;
-})";
-    in_value["input"] = "";
-    in_value["cpu_limit"] = 1;
-    in_value["mem_limit"] = 1024 * 1024 * 2;
+    if (argc != 2)
+    {
+        Usage(argv[0]);
+        return -1;
+    }
 
-    Json::FastWriter writer;
-    in_json = writer.write(in_value);
+    Server server;
 
-    std::cout << in_json << std::endl;
+    server.Post("/compile_and_run", [](const Request &req, Response &resp) {
+        std::string in_json = req.body;
+        std::string out_json;
+        if (!in_json.empty())
+        {
+            CompileAndRun::Start(in_json, &out_json);
+            resp.set_content(out_json, "application/json;charset=utf-8");
+        }
+    });
 
-    std::string out_json;
-    CompileAndRun::Start(in_json, &out_json);
-
-    std::cout << out_json << std::endl;
+    server.listen("0.0.0.0", atoi(argv[1]));
 
     return 0;
 }
